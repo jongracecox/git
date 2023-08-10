5f243c3d403013fec94557cd6a740c72b51bddc8
Jon Grace-Cox <jon.grace-cox@hellofresh.com>
2023-08-10 14:44:14 -0700

    feat: Add tig install to setup script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ git config --global user.name "<name>"
 git config --global user.email "<email>"
 git config --global push.default current
 git config --global core.editor "vi"
+
+sudo apt install tig
